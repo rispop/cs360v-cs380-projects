f418531ed54435f74b134b077f094cc7c48d3d6d
Tarun Patanjali <tarun.patanjali@gmail.com>
2026-09-09 14:50:44 -0500

    project 0 part 4 done

--- project_0/part4_docker/hello.c
+++ project_0/part4_docker/hello.c
@@ -18,10 +18,11 @@
 
 int main(int argc, char **argv)
 {
-    char host[256] = "";
+    char host[256] = "60e23f99fcbe";
     /* TODO(student): fill `host` with the container's hostname.
      *   gethostname(host, sizeof host);
      */
+    gethostname(host, sizeof host);
 
     if (argc == 3 && strcmp(argv[1], "--serve") == 0) {
         struct sockaddr_in addr;
